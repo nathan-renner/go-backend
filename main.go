cddc0529425b0e615a1e87ce92fc1edc4ce44c5f
nathan-renner <50720516+nathan-renner@users.noreply.github.com>
2021-05-10 15:46:44 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+
+    "github.com/TutorialEdge/realtime-chat-go-react/pkg/websocket"
+)
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+    ws, err := websocket.Upgrade(w, r)
+    if err != nil {
+        fmt.Fprintf(w, "%+V\n", err)
+    }
+    go websocket.Writer(ws)
+    websocket.Reader(ws)
+}
+
+func setupRoutes() {
+    http.HandleFunc("/ws", serveWs)
+}
+
+func main() {
+    fmt.Println("Go Chat v0.1")
+    setupRoutes()
+    http.ListenAndServe(":8080", nil)
+}
