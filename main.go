54eac18dc6ab6c379bbb473dbbb1997add9056d7
nathan-renner <50720516+nathan-renner@users.noreply.github.com>
2021-05-15 11:00:07 -0400

    Added ability to handle multiple clients

--- main.go
+++ main.go
@@ -2,22 +2,181 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
 
-    "github.com/nathan-renner/go-backend/pkg/websocket"
+    "github.com/gorilla/websocket"
 )
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
-    ws, err := websocket.Upgrade(w, r)
+// Server functions **********************************************************************
+
+var upgrader = websocket.Upgrader{
+  ReadBufferSize:  1024,
+  WriteBufferSize: 1024,
+
+  CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+    ws, err := upgrader.Upgrade(w, r, nil)
     if err != nil {
-        fmt.Fprintf(w, "%+V\n", err)
+        log.Println(err)
+        return ws, err
     }
-    go websocket.Writer(ws)
-    websocket.Reader(ws)
+    return ws, nil
+}
+
+// func Reader(conn *websocket.Conn) {
+//     for {
+//     // read in a message
+//         messageType, p, err := conn.ReadMessage()
+//         if err != nil {
+//             log.Println(err)
+//             return
+//         }
+//     // print out that message for clarity
+//         fmt.Println(string(p))
+
+//         if err := conn.WriteMessage(messageType, p); err != nil {
+//             log.Println(err)
+//             return
+//         }
+
+//     }
+// }
+
+// func Writer(conn *websocket.Conn) {
+//     for {
+//         fmt.Println("Sending")
+//         messageType, r, err := conn.NextReader()
+//         if err != nil {
+//             fmt.Println(err)
+//             return
+//         }
+//         w, err := conn.NextWriter(messageType)
+//         if err != nil {
+//             fmt.Println(err)
+//             return
+//         }
+//         if _, err := io.Copy(w, r); err != nil {
+//             fmt.Println(err)
+//             return
+//         }
+//         if err := w.Close(); err != nil {
+//             fmt.Println(err)
+//             return
+//         }
+//     }
+// }
+
+// Pool Logic *************************************************************************
+
+type Pool struct {
+    Register   chan *Client
+    Unregister chan *Client
+    Clients    map[*Client]bool
+    Broadcast  chan Message
+}
+
+func NewPool() *Pool {
+    return &Pool{
+        Register:   make(chan *Client),
+        Unregister: make(chan *Client),
+        Clients:    make(map[*Client]bool),
+        Broadcast:  make(chan Message),
+    }
+}
+
+// Client Logic **********************************************************************
+
+type Client struct {
+    ID   string
+    Conn *websocket.Conn
+    Pool *Pool
+}
+
+type Message struct {
+    Status bool `json:"status"`
+    Type int    `json:"type"`
+    Body string `json:"body"`
+    //Username string `json:"username"`
+}
+
+func (c *Client) Read() {
+    defer func() {
+        c.Pool.Unregister <- c
+        c.Conn.Close()
+    }()
+
+    for {
+        messageType, p, err := c.Conn.ReadMessage()
+        if err != nil {
+            log.Println(err)
+            return
+        }
+        message := Message{Type: messageType, Body: string(p)}
+        c.Pool.Broadcast <- message
+        fmt.Printf("Message Received: %+v\n", message)
+    }
+}
+
+// on Start Logic ********************************************************************
+
+func (pool *Pool) Start() {
+    for {
+        select {
+        case client := <-pool.Register: // edit this function to take in name
+            pool.Clients[client] = true
+            fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+            for client, _ := range pool.Clients {
+                fmt.Println(client)
+                client.Conn.WriteJSON(Message{Status: true, Type: 1, Body: "New User Joined..."})
+            }
+            break
+        case client := <-pool.Unregister:
+            delete(pool.Clients, client)
+            fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+            for client, _ := range pool.Clients {
+                client.Conn.WriteJSON(Message{Status: true, Type: 1, Body: "User Disconnected..."})
+            }
+            break
+        case message := <-pool.Broadcast:
+            fmt.Println("Sending message to all clients in Pool")
+            for client, _ := range pool.Clients {
+                if err := client.Conn.WriteJSON(message); err != nil {
+                    fmt.Println(err)
+                    return
+                }
+            }
+        }
+    }
+}
+
+// Server hosting ************************************************************************
+
+func serveWs(pool *Pool, w http.ResponseWriter, r *http.Request) {
+    fmt.Println("WebSocket Endpoint Hit")
+    conn, err := Upgrade(w, r)
+    if err != nil {
+        fmt.Fprintf(w, "%+v\n", err)
+    }
+
+    client := &Client{
+        Conn: conn,
+        Pool: pool,
+    }
+
+    pool.Register <- client
+    client.Read()
 }
 
 func setupRoutes() {
-    http.HandleFunc("/ws", serveWs)
+    pool := NewPool()
+    go pool.Start()
+
+    http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+        serveWs(pool, w, r)
+    })
 }
 
 func main() {
