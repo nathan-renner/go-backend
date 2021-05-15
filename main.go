94b9f6335a717937713c9ee09aaa909ae1db445d
nathan-renner <50720516+nathan-renner@users.noreply.github.com>
2021-05-15 11:23:50 -0400

    Added final touches

--- main.go
+++ main.go
@@ -26,49 +26,6 @@ func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
     return ws, nil
 }
 
-// func Reader(conn *websocket.Conn) {
-//     for {
-//     // read in a message
-//         messageType, p, err := conn.ReadMessage()
-//         if err != nil {
-//             log.Println(err)
-//             return
-//         }
-//     // print out that message for clarity
-//         fmt.Println(string(p))
-
-//         if err := conn.WriteMessage(messageType, p); err != nil {
-//             log.Println(err)
-//             return
-//         }
-
-//     }
-// }
-
-// func Writer(conn *websocket.Conn) {
-//     for {
-//         fmt.Println("Sending")
-//         messageType, r, err := conn.NextReader()
-//         if err != nil {
-//             fmt.Println(err)
-//             return
-//         }
-//         w, err := conn.NextWriter(messageType)
-//         if err != nil {
-//             fmt.Println(err)
-//             return
-//         }
-//         if _, err := io.Copy(w, r); err != nil {
-//             fmt.Println(err)
-//             return
-//         }
-//         if err := w.Close(); err != nil {
-//             fmt.Println(err)
-//             return
-//         }
-//     }
-// }
-
 // Pool Logic *************************************************************************
 
 type Pool struct {
